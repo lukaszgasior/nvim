bd4db2a564a9620efda52d24ea16519063876e09
Lukasz Gasior <lukasz@gasior.net.pl>
2023-07-11 08:10:26 +0200

    telescope configuration

--- lua/mrlugas/keymaps.lua
+++ lua/mrlugas/keymaps.lua
@@ -10,3 +10,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
+-- keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
