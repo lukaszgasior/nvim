7e769d9b72039ed3e334e080a9461e4cac2a1b82
Lukasz Gasior <lukasz@gasior.net.pl>
2023-07-11 07:17:21 +0200

    cursor line

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,12 @@
 return {
   "olimorris/onedarkpro.nvim",
   priority = 1000, -- Ensure it loads first
+  config = function()
+    require("onedarkpro").setup({
+        options = {
+          cursorline = true, -- Use cursorline highlighting?
+          transparency = false, -- Use a transparent background?
+        }
+    })
+  end
 }
